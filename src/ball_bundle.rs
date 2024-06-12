3460fa5424640b10ee42cbcf3cc01b90f5b1bcbd
Felix <fedecraft3312@gmail.com>
2024-06-12 19:32:59 -0300

    Add ball to the game

--- src/ball_bundle.rs
+++ src/ball_bundle.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
 
 use crate::Position;
+
+const BALL_SIZE: f32 = 5.;
 
 #[derive(Component)]
 pub struct Ball;
@@ -12,10 +15,10 @@ pub struct BallBundle {
 }
 
 impl BallBundle {
-    pub fn new() -> Self {
+    pub fn new(x: f32, y: f32) -> Self {
         Self {
             ball: Ball,
-            position: Position(Vec2::new(0., 0.))
+            position: Position(Vec2::new(x, y))
         }
     }
 }
@@ -25,8 +28,27 @@ impl BallBundle {
 /// 
 pub fn spawn_ball(
     mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn_empty()
-        .insert(Transform::default())
-        .insert(BallBundle::new());
+    let shape = Mesh::from(Circle::new(BALL_SIZE));
+    let color = ColorMaterial::from(Color::rgb(1., 0., 0.));
+    
+    // `Assets::add` will load these into memory and return a
+    // `Handle` (an ID) to these assets. When all references
+    // to this `Handle` are cleaned up the asset is cleaned up.
+    let mesh_handle = meshes.add(shape);
+    let material_handle = materials.add(color);
+    
+    // Here we are using `spawn` instead of `spawn_empty` 
+    // followed by an `insert`. They mean the same thing, 
+    // letting us spawn many components on a new entity at once.
+    commands.spawn((
+        BallBundle::new(0., 0.),
+        MaterialMesh2dBundle {
+            mesh: mesh_handle.into(),
+            material: material_handle,
+            ..default()
+        }
+    ));
 }
